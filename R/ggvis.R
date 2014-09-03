fd0e4180796b62cc20f1091a1d23b12a7df1a901
davharris <davharris@ucdavis.edu>
2014-09-02 22:11:59 -0700

    fancier tooltips for ggvis

--- R/ggvis.R
+++ R/ggvis.R
@@ -31,7 +31,35 @@ library(ggvis)
 dat = cbind(as.data.frame(xy), cluster = as.factor(km$cluster))
 colnames(dat)[1:2] = c("x", "y")
 
+dat$id = 1:nrow(dat)
+
+# Based on koundy's Stack Overflow answer at
+# http://stackoverflow.com/a/24528087/783153
+all_values <- function(x) {
+  if(is.null(x)) return(NULL)
+  row <- dat[dat$id == x$id, ]
+  
+  paste0(
+    "This is the ",
+    row$id,
+    "th point.<br>It belongs to group ", 
+    row$cluster,
+    '.<br><a href = "www.google.com">Here is a link.</a>'
+  )
+}
+
 dat %>% 
-  ggvis(~x, ~y, fill = ~cluster, shape = ~cluster) %>% 
+  ggvis(~x, ~y, fill = ~cluster, shape = ~cluster, key := ~id) %>% 
   layer_points() %>%
-  add_tooltip(function(x){x$cluster})
+  add_tooltip(all_values, on = "click") %>%
+  add_tooltip(
+    function(x){
+      paste0(
+        "This point belongs to <b>group ",
+        x$cluster,
+        "</b><br>Click for more information"
+      )
+      
+    }, 
+    on = "hover"
+  )
